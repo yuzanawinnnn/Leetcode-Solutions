eeeef89ab49f63f131af8ec3781e09337c2bd8d2
Cherry <56732512+Cherry-theCSStudent@users.noreply.github.com>
2023-05-26 14:41:26 +0630

    Create sumOfMultiples.py

---
+++ sumOfMultiples.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        total = 0
+        for i in range(1,n+1):
+            if(i%3== 0 or i%5==0 or i%7==0):
+                total = total + i
+        return total
