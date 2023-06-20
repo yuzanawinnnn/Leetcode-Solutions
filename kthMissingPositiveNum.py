e87ee0d51aac8d4cdba2bbba6a9151e03e4befab
Cherry <56732512+Cherry-theCSStudent@users.noreply.github.com>
2023-06-20 16:15:06 +0630

    Create kthMissingPositiveNum.py

---
+++ kthMissingPositiveNum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        missing = []
+        i = 1
+        while len(missing)<k:
+            if(i not in arr):
+                missing.append(i)
+            i = i + 1
+        return missing[-1]
